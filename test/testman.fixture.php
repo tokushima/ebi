f8c3a7308c2af70fb0fab4d795ed0b80f5f871b6
tokushima <tokushima@10.0.1.11>
2014-12-23 10:40:57 +0900

    setupはfixture

---
+++ test/testman.fixture.php
@@ -0,0 +1,3 @@
+<?php
+\ebi\Dt::setup();
+
--- test/testman.settings.php
+++ test/testman.settings.php
@@ -1,7 +1,5 @@
 <?php
 \ebi\Conf::set('ebi.Db','autocommit',true);
-\ebi\Dt::setup();
-
 
 \testman\Conf::set('urls',\ebi\Dt::get_urls());
 \testman\Conf::set('ssl-verify',false);
