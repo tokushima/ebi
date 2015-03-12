33288335c09fa2f8a6728d838fe0347e40a86d58
tokushima <rhaco1004@gmail.com>
2015-03-12 18:01:15 +0900

    Paginatorのテスト

---
+++ test/ebi/Dao/paginator.php
@@ -0,0 +1,57 @@
+<?php
+\test\db\Paginator::create_table();
+\test\db\Paginator::find_delete();
+
+for($i=1;$i<=98;$i++){
+	$obj = new \test\db\Paginator();
+	$obj->order($i);
+	$obj->save();
+}
+
+$i = 0;
+$paginator = new \ebi\Paginator(20,1);
+foreach(test\db\Paginator::find($paginator,\ebi\Q::order('id')) as $o){
+	$i++;
+}
+eq(20,$i);
+
+
+$i = 0;
+$paginator = new \ebi\Paginator(20,2);
+foreach(test\db\Paginator::find($paginator,\ebi\Q::order('id')) as $o){
+	$i++;
+}
+eq(20,$i);
+
+
+$i = 0;
+$paginator = new \ebi\Paginator(20,3);
+foreach(test\db\Paginator::find($paginator,\ebi\Q::order('id')) as $o){
+	$i++;
+}
+eq(20,$i);
+
+
+$i = 0;
+$paginator = new \ebi\Paginator(20,4);
+foreach(test\db\Paginator::find($paginator,\ebi\Q::order('id')) as $o){
+	$i++;
+}
+eq(20,$i);
+
+
+$i = 0;
+$paginator = new \ebi\Paginator(20,5);
+foreach(test\db\Paginator::find($paginator,\ebi\Q::order('id')) as $o){
+	$i++;
+}
+eq(18,$i);
+
+
+$i = 0;
+$paginator = new \ebi\Paginator(20,6);
+foreach(test\db\Paginator::find($paginator,\ebi\Q::order('id')) as $o){
+	$i++;
+}
+eq(0,$i);
+
