e10abc47d7e5e9c770eb8d2cb77022d81c5222ef
tokushima <tokushima@10.0.1.11>
2015-05-20 22:23:18 +0900

    呼べばいいだけだった

--- src/ebi/Dt/cmd/dao_create_table.php
+++ src/ebi/Dt/cmd/dao_create_table.php
@@ -7,15 +7,16 @@
 
 $model_list = [];
 
+if(!empty($model)){
+	$model = str_replace('.','\\',$model);
+	class_exists($model);
+}
 foreach(\ebi\Dt::classes('\ebi\Dao') as $class_info){
 	$r = new \ReflectionClass($class_info['class']);
 	if($r->getParentClass()->getName() == 'ebi\Dao'){
 		$model_list[] = $r->getName();
 	}
 }
-
-$model = str_replace('.','\\',$model);
-
 foreach($model_list as $m){
 	if(empty($model) || strpos($m,$model) !== false){
 		if($drop === true && call_user_func([$m,'drop_table'])){
