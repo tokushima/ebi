6b2c4fba548dece17601f4277fbae635742b0207
tokushima <tokushima@127.0.0.1>
2015-09-30 17:52:10 +0900

    start使った事ない

--- src/ebi/Dt.php
+++ src/ebi/Dt.php
@@ -665,30 +665,7 @@ class Dt{
 		}
 		throw new \LogicException('指定のメールが飛んでいない > ['.$to.'] '.$keyword);
 	}
-
-	/**
-	 * entryを探しhtaccessを生成する
-	 * @param string $base
-	 */
-	public static function htaccess($base){
-		if(substr($base,0,1) !== '/') $base = '/'.$base;
-		$rules = "RewriteEngine On\nRewriteBase ".$base."\n\n";
-		foreach(new \DirectoryIterator(getcwd()) as $f){
-			if($f->isFile() && substr($f->getPathname(),-4) == '.php' && substr($f->getFilename(),0,1) != '_' && $f->getFilename() != 'index.php'){
-				$src = file_get_contents($f->getPathname());
-				if(strpos($src,'Flo'.'w::app(') !== false){
-					$app = substr($f->getFilename(),0,-4);
-					$rules .= "RewriteCond %{REQUEST_FILENAME} !-f\nRewriteCond %{REQUEST_FILENAME} !-d\nRewriteRule ^".$app."[/]{0,1}(.*)\$ ".$app.".php/\$1?%{QUERY_STRING} [L]\n\n";
-				}
-			}
-		}
-		if(is_file(getcwd().'/index.php')){
-			$rules .= "RewriteCond %{REQUEST_FILENAME} !-f\nRewriteRule ^(.*)\$ index.php/\$1?%{QUERY_STRING} [L]\n\n";
-		}
-		file_put_contents('.htaccess',$rules);
-
-		return [realpath('.htaccess'),$rules];
-	}
+	
 	/**
 	 * アプリケーションモードに従い初期処理を行うファイルのパス
 	 * @return string
