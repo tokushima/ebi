03140a78d05740c62212622c1c2d4704e0ff120a
tokushima <tokushima@10.0.1.11>
2015-12-15 01:09:56 +0900

    ログのpluginをlog_outputのみに変更

--- src/ebi/LogMailSender.php
+++ src/ebi/LogMailSender.php
@@ -15,39 +15,7 @@ namespace ebi;
 class LogMailSender{
 	private $template_base;
 
-	/**
-	 * @plugin \ebi\Log
-	 * @param \ebi\Log $log
-	 * @param string $id
-	 */
-	public function debug(\ebi\Log $log){
-		$this->send('debug',$log);
-	}
-	/**
-	 * @plugin \ebi\Log
-	 * @param \ebi\Log $log
-	 * @param string $id
-	 */
-	public function info(\ebi\Log $log){
-		$this->send('info',$log);
-	}
-	/**
-	 * @plugin \ebi\Log
-	 * @param \ebi\Log $log
-	 * @param string $id
-	 */
-	public function warn(\ebi\Log $log){
-		$this->send('warn',$log);
-	}
-	/**
-	 * @plugin \ebi\Log
-	 * @param \ebi\Log $log
-	 * @param string $id
-	 */
-	public function error(\ebi\Log $log){
-		$this->send('error',$log);
-	}
-	protected function send($level,\ebi\Log $log){
+	public function log_output(\ebi\Log $log){
 		if(empty($this->template_base)){
 			/**
 			 * mailテンプレートのディレクトリ (debug.xml, info.xml, warn.xml, error.xml)
@@ -55,7 +23,7 @@ class LogMailSender{
 			 */
 			$this->template_base = \ebi\Conf::get('template_dir',\ebi\Conf::resource_path('log_mail'));
 		}
-		$template = \ebi\Util::path_absolute($this->template_base,$level.'.xml');
+		$template = \ebi\Util::path_absolute($this->template_base,$log->fm_level().'.xml');
 		
 		if(is_file($template)){
 			$mail = new \ebi\Mail();			
