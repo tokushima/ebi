caea2a4869f4855ad415a9a85a1c25b7fe3f6713
tokushima <tokushima@192.168.103.193>
2016-01-25 16:02:11 +0900

    pear探すのはやめる

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,6 @@ $dir = dirname(dirname(dirname(__DIR__))).'/lib';
 if(is_dir($dir) && strpos(get_include_path(),$dir) === false){
 	set_include_path($dir.PATH_SEPARATOR.get_include_path());
 }
-if(is_dir($libdir=dirname(dirname(dirname(__DIR__))).'/pear') && strpos(get_include_path(),$libdir) === false){
-	set_include_path(get_include_path().PATH_SEPARATOR.$libdir);
-}
 
 include_once(__DIR__.'/autoload.php');
 
