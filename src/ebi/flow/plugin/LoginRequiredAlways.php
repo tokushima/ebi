f7d8bf2c38a408a8948e067a92244548bbbf1450
tokushima <tokushima@192.168.103.193>
2016-04-28 19:15:01 +0900

    UnauthorizedExceptionに変更

--- src/ebi/flow/plugin/LoginRequiredAlways.php
+++ src/ebi/flow/plugin/LoginRequiredAlways.php
@@ -12,7 +12,7 @@ class LoginRequiredAlways{
 	public function before_login_required(\ebi\flow\Request $req){
 		if(!$req->is_login()){
 			\ebi\HttpHeader::send_status(401);
-			throw new \ebi\exception\LogicException('Unauthorized');
+			throw new \ebi\exception\UnauthorizedException('Unauthorized');
 		}
 	}
 }
