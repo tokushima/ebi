1140b6d7b83bf96fe27c4df57586442b62ca3471
tokushima <tokushima@10.0.1.11>
2016-06-19 23:36:40 +0900

    書き込みモードにしてなかった

--- src/ebi/LogStdout.php
+++ src/ebi/LogStdout.php
@@ -13,11 +13,11 @@ class LogStdout{
 	 */
 	public function log_output(\ebi\Log $log){
 		if($log->level() > 3 || $log->level() == -1){
-			$fp = fopen('php://stdout','rb');
+			$fp = fopen('php://stdout','wb');
 				fwrite($fp,(string)$log);
 			fclose($fp);
 		}else{
-			$fp = fopen('php://stderr','rb');
+			$fp = fopen('php://stderr','wb');
 				fwrite($fp,(string)$log);
 			fclose($fp);
 		}
