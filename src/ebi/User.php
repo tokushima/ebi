e4938c2cf19de4f03e767846b5cd6557aae6460f
tokushima <tokushima@10.0.1.11>
2016-08-02 00:48:00 +0900

    add

---
+++ src/ebi/User.php
@@ -0,0 +1,17 @@
+<?php
+namespace ebi;
+/**
+ * ユーザモデル
+ * @author tokushima
+ *
+ */
+class User{
+	private $id;
+	
+	public function __construct($id){
+		$this->id = $id;
+	}
+	public function id(){
+		return $this->id();
+	}
+}
---
+++ src/ebi/flow/plugin/SimpleAuth.php
@@ -0,0 +1,23 @@
+<?php
+namespace ebi\flow\plugin;
+/**
+ * ユーザ認証
+ * @author tokushima
+ *
+ */
+class SimpleAuth{
+	public function login_condition(\ebi\flow\Request $req){
+		$users = \ebi\Conf::get('users',[]);
+		
+		foreach($users as $user_name => $password){
+			if(
+				$user_name == $req->in_vars('user_name') &&
+				$password == $req->in_vars('password')
+			){
+				$req->user(new \ebi\User($user_name));
+				return true;
+			}
+		}
+		return false;
+	}
+}
