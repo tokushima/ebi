91b04321a615bdc068983a40f866cc38556fae7c
tokushima <tokushima@10.0.1.1>
2017-02-01 13:51:08 +0900

    ログインさせたい場合のFlow base

---
+++ lib/ebi/flow/UserRequest.php
@@ -0,0 +1,10 @@
+<?php
+namespace ebi\flow;
+/**
+ * ログインが必要なリクエストやセッションを処理するactionベース
+ * @author tokushima
+ * @login ログイン @['type'=>'ebi.User']
+ */
+class UserRequest extends \ebi\flow\Request{
+}
+
