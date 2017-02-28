9522a6333ad91176951ddf798babb304e86daf50
tokushima <tokushima@10.0.1.1>
2017-02-28 12:55:50 +0900

    remember me model

--- lib/ebi/UserRememberMeDao.php
+++ lib/ebi/UserRememberMeDao.php
@@ -2,13 +2,13 @@
 namespace ebi;
 use \ebi\Q;
 /**
+ * Remember me
  * @var serial $id
  * @var integer $user_id
- * @var string $token @['auto_code_add'=>true,'max'=>64]
- * @var string $key @['auto_code_add'=>true,'max'=>20]
+ * @var string $token @['auto_code_add'=>true,'max'=>80]
+ * @var string $key @['auto_code_add'=>true,'max'=>45]
  * @var timestamp $expire_date
  * @author tokushima
- * TODO
  */
 class UserRememberMeDao extends \ebi\Dao{
 	protected $id;
@@ -18,27 +18,77 @@ class UserRememberMeDao extends \ebi\Dao{
 	protected $expire_date;
 	
 	private static function encrypt($user_id){
-		return sha1(\ebi\Conf::get('salt').$user_id);
+		return hash('sha256',(\ebi\Conf::get('salt',static::class).$user_id),false);
 	}
+	
+	/**
+	 * Userをセットする
+	 * @param \ebi\flow\Request $req
+	 */
 	public static function set(\ebi\flow\Request $req){
 		if($req->user() instanceof \ebi\User){
-			$expire = time() + 5184000; // 2day
-			
 			try{
 				$self = static::find_get(Q::eq('user_id',$req->user()->id()));
 			}catch(\ebi\exception\NotFoundException $e){
 				$self = new static();
 				$self->user_id($req->user()->id());
 			}
-			$self->expire_date($expire);
-			$self->save();
+			$lifetime = time() + \ebi\Conf::get('lifetime',5184000); // 60day
+			$codebase = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#%&@.';
 			
-			$req->write_cookie('_lt',$self->token(),$expire);
-			$req->write_cookie('_sk',$self->key(),$expire);
-			$req->write_cookie('_ui',self::encrypt($self->user_id()),$expire);
+			for($i=0;$i<100;$i++){
+				$token = \ebi\Code::rand($codebase,80);
+				$key = \ebi\Code::rand($codebase,45);
+				
+				if(static::find_count(Q::eq('token',$token),Q::eq('key',$key)) === 0){
+					break;
+				}
+				$token = '';
+			}
+			if(!empty($token)){
+				$self->token($token);
+				$self->key($key);
+				$self->expire_date(time() + $lifetime);
+				$self->save();
+			
+				$req->write_cookie('_lt',$self->token(),$lifetime);
+				$req->write_cookie('_sk',$self->key().'/'.self::encrypt($self->user_id()),$lifetime);
+			}
 		}
 	}
+	/**
+	 * user_idを取得する
+	 * @param \ebi\flow\Request $req
+	 * @throws \ebi\exception\NotFoundException
+	 * @return string
+	 */
 	public static function get(\ebi\flow\Request $req){
-
+		if(isset($_COOKIE['_lt'])){
+			if(rand(1,10) == 5){
+				foreach(static::find(Q::lt('expire_date',time()),new Paginator(10)) as $obj){
+					$obj->delete();
+				}
+			}
+			$token = $_COOKIE['_lt'];
+			$sk = explode('/',isset($_COOKIE['_sk']) ? $_COOKIE['_sk'] : null);
+			
+			if(isset($sk[1])){
+				list($key,$id) = $sk;
+				
+				try{
+					$self = static::find_get(
+						Q::eq('token',$token),
+						Q::eq('key',$key),
+						Q::gt('expire_date',time())
+					);
+					
+					if(self::encrypt($self->user_id()) === $id){
+						return $self->user_id();
+					}
+				}catch(\ebi\exception\NotFoundException $e){
+				}
+			}
+		}
+		throw new \ebi\exception\NotFoundException();
 	}
 }
