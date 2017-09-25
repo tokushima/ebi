9debd713f6509eb6d29890af49d97e7d3c043d6d
tokushima <tokushima@192.168.11.10>
2017-09-25 22:43:44 +0900

    move

--- lib/ebi/Util.php
+++ lib/ebi/Util.php
@@ -462,44 +462,6 @@ class Util{
 	}
 	
 	/**
-	 * JPGを指定のサイズにリサイズする
-	 * @param string $filename
-	 * @param string $out_filename
-	 * @param integer $width
-	 * @param integer $height
-	 * @param integer $quality
-	 * @return integer[] dst_width,dst_height
-	 */
-	public static function image_jpeg_resize($filename,$out_filename,$width,$height,$quality=100){
-		$src_image = imagecreatefromjpeg($filename);
-		$src_width = imagesx($src_image);
-		$src_height = imagesy($src_image);
-		
-		$wp = ($width > $src_width) ? 1 : ($width / $src_width);
-		$hp = ($height > $src_height) ? 1 : ($height / $src_height);
-		
-		if($wp == 1 && $hp == 1){
-			$xp = 1;
-		}else if($wp == 1 && $hp < 1){
-			$wp = $hp;
-		}else if($wp < 1 && $hp == 1){
-			$hp = $wp;
-		}
-		$xp = ($wp > $hp) ? $hp : $wp;
-		
-		$dst_width = floor($src_width * $xp);
-		$dst_height = floor($src_height * $xp);
-		
-		$dst_image = imagecreatetruecolor($dst_width,$dst_height);
-		imagecopyresampled($dst_image,$src_image,0,0,0,0,$dst_width,$dst_height,$src_width,$src_height);
-		
-		imagedestroy($src_image);
-		imagejpeg($dst_image,$out_filename,$quality);
-		imagedestroy($dst_image);
-		
-		return [$dst_width,$dst_height];
-	}
-	/**
 	 * 改行付きで出力
 	 * @param string $val
 	 */
