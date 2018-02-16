fb8ce2b4de572bb7a92498bf6fcf927843d6f953
tokushima <tokushima@192.168.103.193>
2018-02-16 11:44:05 +0900

    Image

--- lib/ebi/Image.php
+++ lib/ebi/Image.php
@@ -1,74 +1,192 @@
 <?php
 namespace ebi;
-
+/**
+ * 
+ * @author tokushima
+ *
+ */
 class Image{
+	/**
+	 * 縦向き
+	 * @var integer
+	 */
+	const ORIENTATION_PORTRAIT = 1;
+	/**
+	 * 横向き
+	 * @var integer
+	 */
+	const ORIENTATION_LANDSCAPE = 2;
+	/**
+	 * 正方形
+	 * @var integer
+	 */
+	const ORIENTATION_SQUARE = 3;
+	
 	private $canvas;
 	private $mode;
 	
-	public static function jpeg($filename){
-		$self = new static();
-		
-		if(false && extension_loaded('imagick')){
-			$self->canvas = new \Imagick($filename);
-			$self->mode = 1;
-		}else{
-			$self->canvas = imagecreatefromjpeg($filename);
-			$self->mode = 2;
-		}
-		\ebi\Log::trace($self->mode);
-		return $self;
-	}
-	public static function jpeg_string($string){
-		$self = new static();
-		
-		if(extension_loaded('imagick')){
-			$self->canvas = new \Imagick();
-			$self->canvas->readImageBlob($string);
-			$self->mode = 1;
-		}else{
-			$self->canvas = imagecreatefromstring($string);
-			$self->mode = 2;
-		}
-		return $self;
-	}
-	
-	public function __destruct(){
-		if($this->mode == 1){
-			$this->canvas->clear();			
-		}else{
-			imagedestroy($this->canvas);
-		}
-	}
-	
-	public function output($filename=null){
-		if($this->mode == 1){
-			if(empty($filename)){
-				header('Content-Type: image/jpeg');
-				print($this->canvas);
-			}else{
-				$this->canvas->writeImage($filename);
-			}
-		}else{
-			if(empty($filename)){
-				header('Content-Type: image/jpeg');
-				imagejpeg($this->canvas);
-			}else{
-				imagejpeg($this->canvas,$filename);
+	/**
+	 * 
+	 * @param string $filename
+	 */
+	public function __construct($filename){
+		if($filename != __FILE__){
+			try{
+				if(extension_loaded('imagick')){
+					$this->canvas = new \Imagick($filename);
+					$this->mode = 1;
+				}else{
+					$size = getimagesize($filename);
+					
+					switch($size['mime']){
+						case  'image/jpeg':
+							$this->canvas = imagecreatefromjpeg($filename);
+							break;
+						case 'image/png':
+							$this->canvas = imagecreatefrompng($filename);
+							break;
+						case 'image/gif':
+							$this->canvas = imagecreatefromgif($filename);
+							break;
+						default:
+							throw new \ebi\exception\ImageException();
+					}
+					$this->mode = 2;
+				}
+			}catch(\Exception $e){
+				throw new \ebi\exception\ImageException();
 			}
 		}
 	}
 	
 	/**
-	 * JPG画像ファイルを指定のサイズで縮小・切り取り
-	 * 
-	 * @param string $filename 入力ファイル名
-	 * @param integer $width 幅(px)
-	 * @param integer $height 高さ(px)
-	 * @param string $output 出力先ファイル名
+	 * バイナリ文字列から画像を読み込む
+	 * @param string $string
+	 * @return \ebi\Image
+	 */
+	public static function readImageBlob($string){
+		$self = new static(__FILE__);
+		
+		try{
+			if(extension_loaded('imagick')){
+				$self->canvas = new \Imagick();
+				if($self->canvas->readImageBlob($string) !== true){
+					throw \ebi\exception\ImageException();
+				}
+				$self->mode = 1;
+			}else{
+				$self->canvas = imagecreatefromstring($string);
+				
+				if($self->canvas === false){
+					throw \ebi\exception\ImageException();
+				}
+				$self->mode = 2;
+			}
+		}catch(\Exception $e){
+			throw \ebi\exception\ImageException();
+		}
+		return $self;
+	}
+	
+	
+	/**
+	 * ファイルに書き出す
+	 * @param string $filename
+	 * @return \ebi\Image
+	 */
+	public function write($filename){
+		\ebi\Util::mkdir(dirname($filename));
+		
+		if($this->mode == 1){
+			$this->canvas->writeImage($filename);
+		}else{
+			$type = 'jpg';
+			
+			if(preg_match('/\.([\w]+)$/',$filename,$m)){
+				$type = strtolower($m[1]);
+			}
+			switch($type){
+				case 'jpeg':
+				case 'jpg':
+					imagejpeg($this->canvas,$filename);
+					break;
+				case 'png':
+					imagepng($this->canvas,$filename);
+					break;
+				case 'gif':
+					imagegif($this->canvas,$filename);
+					break;
+				default:
+					imagejpeg($this->canvas,$filename);
+			}
+		}
+		return $this;
+	}	
+	
+	/**
+	 * 画像をブラウザに出力する
+	 * @return \ebi\Image
+	 */
+	public function output($format='jpeg'){
+		$format = strtolower($format);
+	
+		switch($format){
+			case 'png':
+				header('Content-Type: image/png');
+				break;
+			case 'gif':
+				header('Content-Type: image/gif');
+				break;
+			default:
+				header('Content-Type: image/jpeg');
+				$format = 'jpeg';
+		}
+		
+		if($this->mode == 1){
+			$this->canvas->setImageFormat($format);
+			print($this->canvas);
+		}else{
+			switch($format){
+				case 'jpeg':
+					imagejpeg($this->canvas);
+					break;
+				case 'png':
+					imagepng($this->canvas);
+					break;
+				case 'gif':
+					imagegif($this->canvas);
+					break;
+				default:
+					imagejpeg($this->canvas);
+			}
+		}
+		exit;
+	}
+	
+	public function __destruct(){
+		if($this->mode == 1){
+			$this->canvas->clear();
+		}else{
+			imagedestroy($this->canvas);
+		}
+	}
+	
+	/**
+	 * 画像の一部を抽出する
+	 * @param integer $width 抽出する幅
+	 * @param integer $height 抽出する高さ
+	 * @param integer $x 抽出する領域の左上の X 座標
+	 * @param integer $y 抽出する領域の左上の Y 座標
+	 * @throws \ebi\exception\ImageException
+	 * @return \ebi\Image
 	 */
 	public function crop($width,$height,$x=null,$y=null){
 		if($x === null || $y === null){
-			list($x,$y) = $this->crop_xy($width, $height);
+			list($w,$h) = $this->get_size();
+			$x = ($w - $width) / 2;
+			$y = ($h - $height) / 2;
+			
+			list($x,$y) = [($x >= 0) ? $x : 0,($y >= 0) ? $y : 0];
 		}
 		if($this->mode == 1){
 			$this->canvas->cropImage($width,$height,$x,$y);
@@ -76,51 +194,83 @@ class Image{
 			$canvas = imagecrop($this->canvas, ['x'=>$x,'y'=>$y,'width'=>$width,'height'=>$height]);
 			
 			if($canvas === false){
-				throw new \LogicException();
+				throw new \ebi\exception\ImageException();
 			}
-			
 			imagedestroy($this->canvas);
 			$this->canvas = $canvas;
 		}
 		return $this;
 	}
-	private function crop_xy($width,$height){
+	/**
+	 * サイズ
+	 * @return integer[] width,height
+	 */
+	public function get_size(){
 		if($this->mode == 1){
-			$ow = $this->canvas->getImageWidth();
-			$oh = $this->canvas->getImageHeight();
+			$w = $this->canvas->getImageWidth();
+			$h = $this->canvas->getImageHeight();
 		}else{
-			$ow = imagesx($this->canvas);
-			$oh = imagesy($this->canvas);
+			$w = imagesx($this->canvas);
+			$h = imagesy($this->canvas);
 		}
-		$x = ($ow - $width) / 2;
-		$y = ($oh - $height) / 2;
-		
-		return [($x >= 0) ? $x : 0,($y >= 0) ? $y : 0];
+		return [$w,$h];
 	}
 	
+	/**
+	 * 画像のサイズを変更する
+	 * @param integer $width 変更後の幅
+	 * @param integer $height 変更後の高さ
+	 * @throws \ebi\exception\ImageException
+	 * @return \ebi\Image
+	 */
 	public function resize($width,$height){
-		// TODO
+		list($w,$h) = $this->get_size();
+		
+		$aw = $width / $w;
+		$ah = $height / $h;
+		$a = max($aw,$ah);
+		
+		$cw = $w * $a;
+		$ch = $h * $a;
+		
 		if($this->mode == 1){
-			$this->canvas->scaleImage($width,$height);
+			$this->canvas->scaleImage($cw,$ch);
 		}else{
-			$canvas = imagecreatetruecolor($width,$height);
-			imagecopyresampled($canvas,$this->canvas,0,0,0,0,$width,$height,imagesx($this->canvas),imagesy($this->canvas));
-			
+			$canvas = imagecreatetruecolor($cw,$ch);
+			if(false === imagecopyresampled($canvas,$this->canvas,0,0,0,0,$cw,$ch,$w,$h)){
+				throw new \ebi\exception\ImageException();
+			}			
 			imagedestroy($this->canvas);
 			$this->canvas = $canvas;
 		}
 		return $this;
 	}
-
-
-	private function get_resize_info($original_w,$original_h,$width,$height){
-		$aw = $width / $original_w;
-		$ah = $height / $original_h;
-		$a = max($aw,$ah);
+	
+	/**
+	 * 切り取ってサムネイルを作成する
+	 * @param integer $width 幅
+	 * @param integer $height 高さ
+	 * @return \ebi\Image
+	 */
+	public function thumbnail($width,$height){
+		return $this->resize($width, $height)->crop($width, $height);
+	}
+	
+	/**
+	 * 画像の向き
+	 * @return  integer
+	 */
+	public function get_orientation(){
+		list($w,$h) = $this->get_size();
 		
-		$cw = $original_w * $a;
-		$ch = $original_h * $a;
+		$d = $h / $w;
 		
-		return [$cw,$ch];
+		if($d <= 1.02 && $d >= 0.98){
+			return self::ORIENTATION_SQUARE;
+		}else if($d > 1){
+			return self::ORIENTATION_PORTRAIT;
+		}else{
+			return self::ORIENTATION_LANDSCAPE;
+		}
 	}
 }
--- tests/test/ebi/Image/crop.php
+++ tests/test/ebi/Image/crop.php
@@ -1,32 +1,17 @@
 <?php
 $filename = \testman\Resource::path('test.jpg');
-//$out = \ebi\WorkingStorage::path(base64_encode(__FILE__).'.jpg');
-$out = \ebi\Conf::work_path(base64_encode(__FILE__).'.jpg');
+$out = \ebi\WorkingStorage::path(base64_encode(__FILE__).'.jpg');
 
-$image = \ebi\Image::jpeg($filename);
-$image->crop(100,50)->output($out);
-
-
-
-
-
-
-// \ebi\Image::cropping_jpeg($image_file, 100, 50,$out);
+$image = new \ebi\Image($filename);
+eq(\ebi\Image::ORIENTATION_SQUARE,$image->get_orientation());
+$image->crop(100,50)->write($out);
+eq(\ebi\Image::ORIENTATION_LANDSCAPE,$image->get_orientation());
 
 eq(true,
-	(file_get_contents(\testman\Resource::path('test_crop_gd.jpg')) == file_get_contents($out)) ||
-	(file_get_contents(\testman\Resource::path('test_crop_im.jpg')) == file_get_contents($out))
+	(file_get_contents(\testman\Resource::path('crop_gd.jpg')) == file_get_contents($out)) ||
+	(file_get_contents(\testman\Resource::path('crop_im.jpg')) == file_get_contents($out))
 );
 
-
-
-// $image_src = file_get_contents($image_file);
-// \ebi\Image::cropping_jpeg_from_string($image_src, 100, 50,$out);
-
-// eq(true,
-// 	(file_get_contents(\testman\Resource::path('test_crop_gd.jpg')) == file_get_contents($out)) ||
-// 	(file_get_contents(\testman\Resource::path('test_crop_im.jpg')) == file_get_contents($out))
-// );
 
 
 
