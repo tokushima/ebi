b3df5fd9d5f8c0da628f125edfe9a3285c639b0d
tokushima <tokushima@192.168.103.193>
2018-02-27 12:38:49 +0900

    よく使う

---
+++ lib/ebi/DaoBasicProps.php
@@ -0,0 +1,13 @@
+<?php
+namespace ebi;
+/**
+ * @var serial $id
+ * @var timestamp $create_date 作成日 @['auto_now_add'=>true,'hash'=>false]
+ * @var timestamp $update_date 更新日 @['auto_now'=>true]
+ * @author tokushima
+ */
+trait DaoBasicProps{
+	protected $id;
+	protected $create_date;
+	protected $update_date;
+}
