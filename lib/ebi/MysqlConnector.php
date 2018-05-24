5d17d7414dcc089884c9ef3605d3db80f55e834d
tokushima <tokushima@192.168.11.8>
2018-05-25 02:03:56 +0900

    メソッド名変更

--- lib/ebi/MysqlConnector.php
+++ lib/ebi/MysqlConnector.php
@@ -132,9 +132,14 @@ class MysqlConnector extends \ebi\DbConnector{
 	 * SQLエラーを解析し適切なExceptionをthrowする
 	 * @param mixed[] $error_info 0: SQLSTATE エラーコード, 1:ドライバ固有のエラーコード, 2:ドライバ固有のエラーメッセージ
 	 */
-	public function parse_invalid_query_exception(array $error_info){
-		if($error_info[0] == 23000 && $error_info[1] == 1062){
-			throw new \ebi\exception\UniqueException('Duplicate entry');
+	public function error_info(array $error_info){
+		if($error_info[0] == 23000){
+			if($error_info[1] == 1062){
+				throw new \ebi\exception\UniqueException('Duplicate entry');
+			}
+			if($error_info[1] == 1048){
+				throw new \ebi\exception\RequiredException('Entry is required');
+			}
 		}
 	}
 }
