d15434ea88b909d3b63d0d20bd87959264e92a9b
tokushima <tokushima@tolot.co.jp>
2018-05-31 12:31:11 +0900

    bugfix

--- tests/test/ebi/Util/array_range_search.php
+++ tests/test/ebi/Util/array_range_search.php
@@ -15,5 +15,19 @@ eq(100,\ebi\Util::array_range_search(1, $array));
 eq(5,\ebi\Util::array_range_search(1000210, $array));
 
 
+$array = [
+	1=>1,
+	2=>2,
+	3=>3,
+	4=>4,
+	5=>5,
+	6=>6,
+];
+
+eq(1,\ebi\Util::array_range_search(0, $array));
+eq(1,\ebi\Util::array_range_search(1, $array));
+eq(4,\ebi\Util::array_range_search(4, $array));
+eq(6,\ebi\Util::array_range_search(6, $array));
+eq(6,\ebi\Util::array_range_search(7, $array));
 
 
