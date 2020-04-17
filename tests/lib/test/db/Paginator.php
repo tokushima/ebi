700739bf06af824a7ba38ab2714a94992228e570
tokushima <tokushima@localhost>
2020-04-17 23:28:07 +0900

    typo

--- tests/lib/test/db/Paginator.php
+++ tests/lib/test/db/Paginator.php
@@ -3,7 +3,7 @@ namespace test\db;
 /**
  * @var serial $id
  * @var number $order
- * @var timestamp $updated @['auto_add'=>true]
+ * @var timestamp $updated @['auto_now'=>true]
  * @var string $code1 @['auto_code_add'=>true,'ctype'=>'0']
  * @var string $code2 @['auto_code_add'=>true,'ctype'=>'a']
  */
