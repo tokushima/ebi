d9eca7dfc2ac150637f1e98699d07dc41fe11051
tokushima <>
2023-02-06 16:39:44 +0900

    CSSを修正,bootstrapをCDNに変更

--- tests/lib/test/flow/Helper.php
+++ tests/lib/test/flow/Helper.php
@@ -3,7 +3,7 @@ namespace test\flow;
 
 class Helper{
 	public function keyword($text){
-		$text = str_replace('bbb','<span class="badge badge-warning">bbb</span>',$text);
+		$text = str_replace('bbb','<span class="badge text-bg-warning">bbb</span>',$text);
 		
 		return $text;
 	}
