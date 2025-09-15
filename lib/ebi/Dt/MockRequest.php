c27b834f02cc01fd1a820a4b59e2c44decab0682
tokushima <tokushima@tolot.co.jp>
2025-09-16 02:01:22 +0900

    コメント

--- lib/ebi/Dt/MockRequest.php
+++ lib/ebi/Dt/MockRequest.php
@@ -1,6 +1,23 @@
 <?php
 namespace ebi\Dt;
-
+/**
+ * モック用リクエスト
+ * 継承してモック用のクラスを作成する
+ * 
+ * rewrite_mapで[対象のURLの正規表現パターン => 代わりに実行する自身のメソッド名]を返す
+ * 
+ * エントリファイルmock.phpを作成する
+ * ```
+ * \ebi\Flow::app(
+ * 	\ebi\Dt::mock_flow_mappings()
+ * );
+ * ```
+ * 
+ * commons/local.phpにadd_mockで追加する
+ * ```
+ * \ebi\Dt::add_mock('\yournamespace\Mock');
+ * ```
+ */
 abstract class MockRequest extends \ebi\flow\Request{
 	/**
 	 * [$pattern=>$replacement]
