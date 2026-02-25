3e13dffa7f195f69b789c3d032908999e6152e3b
tokushima <>
2026-02-26 03:46:41 +0900

    最適化

--- lib/ebi/Dt/cmd/check.php
+++ lib/ebi/Dt/cmd/check.php
@@ -1,99 +1,129 @@
 <?php
 /**
  * Check
+ * @param boolean $verbose
  */
 
 $failure = ['db'=>0,'entry'=>0,'mail'=>0,'conf'=>0];
 
-\cmdman\Std::println();
-\cmdman\Std::println_info('Database (Check Existence):');
-\cmdman\Std::println_info(str_repeat('-',50));
-
+// Database
+$db_passed = [];
+$db_errors = [];
 foreach(\ebi\Dt::classes(\ebi\Dao::class) as $class_info){
 	$class_name = \ebi\Util::get_class_name($class_info['class']);
 	try{
 		call_user_func([$class_name,'find_get']);
-		\cmdman\Std::println_success(' o   '.$class_name);
+		$db_passed[] = $class_name;
 	}catch(\ebi\exception\NotFoundException $e){
-		\cmdman\Std::println_success(' o   '.$class_name);
+		$db_passed[] = $class_name;
 	}catch(\ebi\exception\ConnectionException $e){
 		$failure['db']++;
-		\cmdman\Std::println_danger(' x '.$class_name.': '.$e->getMessage());
+		$db_errors[] = ['danger', $class_name.': '.$e->getMessage()];
 	}catch(\Exception $e){
 		$failure['db']++;
-		\cmdman\Std::println_warning(' x '.$class_name.': '.$e->getMessage());
+		$db_errors[] = ['warning', $class_name.': '.$e->getMessage()];
 	}
 }
 
-
 \cmdman\Std::println();
-\cmdman\Std::println_info('Entry (Check mapping):');
-\cmdman\Std::println_info(str_repeat('-',50));
+if(empty($db_errors)){
+	\cmdman\Std::println_success('[Database] '.count($db_passed).' passed');
+}else{
+	\cmdman\Std::println_danger('[Database] '.count($db_passed).' passed, '.$failure['db'].' failed');
+	foreach($db_errors as $err){
+		if($err[0] === 'danger'){
+			\cmdman\Std::println_danger('  [NG] '.$err[1]);
+		}else{
+			\cmdman\Std::println_warning('  [NG] '.$err[1]);
+		}
+	}
+}
+if($verbose){
+	foreach($db_passed as $name){
+		\cmdman\Std::println_success('  [OK] '.$name);
+	}
+}
 
+// Endpoints
+$entry_passed = [];
+$entry_errors = [];
 foreach(\ebi\Util::ls(getcwd(),false,'/\.php$/') as $f){
 	$src = file_get_contents($f->getPathname());
-	
+
 	if(strpos($src,'\ebi\Flow::app(') !== false || strpos($src,'\ebi\App::app(') !== false || strpos($src,'\ebi\App::run(') !== false){
 		$map = \ebi\App::get_map($f->getPathname());
 		$entry = str_replace(getcwd(),'',$f->getPathname());
-		
+
 		foreach($map['patterns'] as $p){
 			if(array_key_exists('action',$p) && is_string($p['action'])){
 				try{
 					[$c, $m] = explode('::',$p['action']);
 					$c = \ebi\Util::get_class_name($c);
-					
+
 					new \ReflectionMethod($c,$m);
-					\cmdman\Std::println_success(' o   '.$entry.' '.$p['name']);
+					$entry_passed[] = $entry.' '.$p['name'];
 				}catch(\ReflectionException $e){
 					$failure['entry']++;
-					\cmdman\Std::println_danger(' x '.$entry.' '.$p['name']);
+					$entry_errors[] = $entry.' '.$p['name'];
 				}
 			}
 		}
 	}
 }
 
-
 \cmdman\Std::println();
-\cmdman\Std::println_info('Config (Check the definition):');
-\cmdman\Std::println_info(str_repeat('-',50));
+if(empty($entry_errors)){
+	\cmdman\Std::println_success('[Endpoints] '.count($entry_passed).' passed');
+}else{
+	\cmdman\Std::println_danger('[Endpoints] '.count($entry_passed).' passed, '.$failure['entry'].' failed');
+	foreach($entry_errors as $err){
+		\cmdman\Std::println_danger('  [NG] '.$err);
+	}
+}
+if($verbose){
+	foreach($entry_passed as $name){
+		\cmdman\Std::println_success('  [OK] '.$name);
+	}
+}
 
+// Config
+$conf_passed = [];
+$conf_undefined_count = 0;
+$conf_errors = [];
 $find_defined_classes = [];
 foreach(\ebi\Dt::classes() as $info){
 	$class_info = \ebi\Dt\SourceAnalyzer::class_info($info['class']);
-	
+
 	if($class_info->has_opt('config_list')){
 		foreach($class_info->opt('config_list') as $info){
 			$key = $class_info->name().'@'.$info->name();
-			
+
 			if(!isset($find_defined_classes[$class_info->name()])){
 				$find_defined_classes[$class_info->name()] = [];
 			}
 			$find_defined_classes[$class_info->name()][] = $info->name();
-			
+
 			if($info->opt('def')){
-				\cmdman\Std::println_success(' o '.$key);
+				$conf_passed[] = $key;
 			}else{
-				\cmdman\Std::println_info(' - '.$key);
+				$conf_undefined_count++;
 			}
 		}
 	}
 }
 
-\cmdman\Std::println();
 foreach(\ebi\Conf::get_defined_keys() as $class => $keys){
 	if(!isset($find_defined_classes[$class])){
 		try{
 			$class_info = \ebi\Dt\SourceAnalyzer::class_info($class,true);
-			
+
 			$find_defined_classes[$class] = [];
 			foreach($class_info->opt('config_list') as $info){
 				$find_defined_classes[$class][] = $info->name();
 			}
 		}catch(\Exception $e){
 			$failure['conf']++;
-			\cmdman\Std::println_danger(' x '.$class);
+			$conf_errors[] = ['danger', $class];
 		}
 	}
 	if(isset($find_defined_classes[$class])){
@@ -101,7 +131,7 @@ foreach(\ebi\Conf::get_defined_keys() as $class => $keys){
 			if(!in_array($key, $find_defined_classes[$class])){
 				if($key !== 'handler'){
 					$failure['conf']++;
-					\cmdman\Std::println_warning(' x '.$class.'@'.$key);
+					$conf_errors[] = ['warning', $class.'@'.$key];
 				}
 			}
 		}
@@ -109,28 +139,53 @@ foreach(\ebi\Conf::get_defined_keys() as $class => $keys){
 }
 
 \cmdman\Std::println();
-\cmdman\Std::println_info('Mail:');
-\cmdman\Std::println_info(str_repeat('-',50));
+$conf_summary = count($conf_passed).' passed';
+if($conf_undefined_count > 0) $conf_summary .= ', '.$conf_undefined_count.' undefined';
+if(!empty($conf_errors)) $conf_summary .= ', '.$failure['conf'].' failed';
 
+if(empty($conf_errors)){
+	\cmdman\Std::println_success('[Config] '.$conf_summary);
+}else{
+	\cmdman\Std::println_danger('[Config] '.$conf_summary);
+	foreach($conf_errors as $err){
+		if($err[0] === 'danger'){
+			\cmdman\Std::println_danger('  [NG] '.$err[1]);
+		}else{
+			\cmdman\Std::println_warning('  [NG] '.$err[1]);
+		}
+	}
+}
+if($verbose){
+	foreach($conf_passed as $key){
+		\cmdman\Std::println_success('  [OK] '.$key);
+	}
+}
+
+// Mail
 $template_list = \ebi\Dt\SourceAnalyzer::mail_template_list();
-$mail_check_result = [];
+$mail_passed = [];
+$mail_errors = [];
 $class_name_max_length = 0;
 foreach(\ebi\Dt::classes() as $class){
 	$class_src = \ebi\Util::file_read($class['filename']);
-	
+
 	foreach($template_list as $mail_info){
 		if(preg_match('/[^\w\/_]'.preg_quote($mail_info->name(),'/').'/',$class_src)){
 			if(\ebi\Dt\SourceAnalyzer::find_mail_doc($mail_info, $class_src)){
-				$mail_check_result[] = [
-					$class['class'],
-					'['.$mail_info->opt('x_t_code').'] '.$mail_info->name(),
-					$mail_info->opt('undefined_vars')
-				];
-				
 				if(!empty($mail_info->opt('undefined_vars'))){
 					$failure['mail']++;
+					$mail_errors[] = [
+						$class['class'],
+						'['.$mail_info->opt('x_t_code').'] '.$mail_info->name(),
+						$mail_info->opt('undefined_vars')
+					];
+				}else{
+					$mail_passed[] = [
+						$class['class'],
+						'['.$mail_info->opt('x_t_code').'] '.$mail_info->name()
+					];
 				}
-				
+
 				if($class_name_max_length < strlen($class['class'])){
 					$class_name_max_length = strlen($class['class']);
 				}
@@ -138,30 +193,41 @@ foreach(\ebi\Dt::classes() as $class){
 		}
 	}
 }
-foreach($mail_check_result as $result){
-	if(empty($result[2])){
-		cmdman\Std::println_success(' o '.str_pad($result[0],$class_name_max_length + 2).$result[1]);
-	}else{
-		cmdman\Std::println_danger(' x '.str_pad($result[0],$class_name_max_length + 2).$result[1].' [ '.implode(', ',$result[2]).' ]');
+
+\cmdman\Std::println();
+if(empty($mail_errors)){
+	\cmdman\Std::println_success('[Mail] '.count($mail_passed).' passed');
+}else{
+	\cmdman\Std::println_danger('[Mail] '.count($mail_passed).' passed, '.$failure['mail'].' failed');
+	foreach($mail_errors as $result){
+		\cmdman\Std::println_danger('  [NG] '.str_pad($result[0],$class_name_max_length + 2).$result[1].' [ '.implode(', ',$result[2]).' ]');
+	}
+}
+if($verbose){
+	foreach($mail_passed as $result){
+		\cmdman\Std::println_success('  [OK] '.str_pad($result[0],$class_name_max_length + 2).$result[1]);
 	}
 }
 
 
 \cmdman\Std::println();
-\cmdman\Std::println();
+\cmdman\Std::println_info(str_repeat('=',50));
 
-if(empty($failure['db']) && 
-	empty($failure['entry']) && 
-	empty($failure['mail']) && 
-	empty($failure['conf'])
-){
-	\cmdman\Std::println_success('Success');
+$total_passed = count($db_passed) + count($entry_passed) + count($conf_passed) + count($mail_passed);
+$total_failed = $failure['db'] + $failure['entry'] + $failure['mail'] + $failure['conf'];
+
+if($total_failed === 0){
+	\cmdman\Std::println_success('  All checks passed ('.$total_passed.' total)');
 }else{
-	\cmdman\Std::println_danger('Failure: '.
-		'Database('.$failure['db'].') '.
-		'Entry('.$failure['entry'].') '.
-		'Mail('.$failure['mail'].') '.
-		'Conf('.$failure['conf'].') '
-	);
+	$failure_details = [];
+	if(!empty($failure['db'])) $failure_details[] = 'Database('.$failure['db'].')';
+	if(!empty($failure['entry'])) $failure_details[] = 'Endpoints('.$failure['entry'].')';
+	if(!empty($failure['mail'])) $failure_details[] = 'Mail('.$failure['mail'].')';
+	if(!empty($failure['conf'])) $failure_details[] = 'Conf('.$failure['conf'].')';
+
+	\cmdman\Std::println_danger('  '.$total_failed.' failed, '.$total_passed.' passed');
+	\cmdman\Std::println_danger('  '.implode(', ', $failure_details));
 }
+
+\cmdman\Std::println_info(str_repeat('=',50));
 \cmdman\Std::println();
