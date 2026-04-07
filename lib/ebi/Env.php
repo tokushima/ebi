bc9ead2f5d17b0d4add875d0234f29142bcd40dd
tokushima <>
2026-04-07 12:11:26 +0900

    最適化

--- lib/ebi/Env.php
+++ lib/ebi/Env.php
@@ -17,16 +17,34 @@ class Env{
 		if(array_key_exists($name,$this->vars)){
 			return $this->vars[$name];
 		}
-		return (isset($_ENV[$name]) && $_ENV[$name] != '') ? 
-			$_ENV[$name] : 
-			(
-				(isset($_SERVER[$name]) && $_SERVER[$name]  != '') ? 
-					$_SERVER[$name] : 
-					(
-						(getenv($name) !== false && getenv($name) != '') ? 
-							getenv($name) : 
-							$default
-					)
-			);
+		if(isset($_ENV[$name]) && $_ENV[$name] != ''){
+			return $_ENV[$name];
+		}
+		if(isset($_SERVER[$name]) && $_SERVER[$name] != ''){
+			return $_SERVER[$name];
+		}
+		if(getenv($name) !== false && getenv($name) != ''){
+			return getenv($name);
+		}
+		switch($name){
+			case 'HOSTNAME':
+			case 'HTTP_HOST':
+			case 'SERVER_NAME':
+				$hostname = gethostname();
+				if($hostname !== false) return $hostname;
+				break;
+			case 'USER':
+			case 'USERNAME':
+			case 'LOGNAME':
+				$user = get_current_user();
+				if($user !== '') return $user;
+				break;
+			case 'PWD':
+			case 'DOCUMENT_ROOT':
+				$cwd = getcwd();
+				if($cwd !== false) return $cwd;
+				break;
+		}
+		return $default;
 	}
 }
