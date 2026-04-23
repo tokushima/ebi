4665a5ee8fa18943b5373c33077bf019f4e39305
tokushima <>
2026-04-24 01:55:18 +0900

    初期化が必要

--- lib/ebi/Exception.php
+++ lib/ebi/Exception.php
@@ -2,7 +2,7 @@
 namespace ebi;
 
 class Exception extends \Exception{
-    protected ?int $http_status;
+    protected ?int $http_status = null;
 
     public function http_status(): ?int{
         return $this->http_status;
